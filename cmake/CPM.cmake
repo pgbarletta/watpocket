34d6c8ec8d9a2c7afa3ef7ccb0645ea91c23d678
pgbarletta <pbarletta@gmail.com>
2026-04-01 14:24:53 -0400

    Fix offline wheel release bootstrap

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -16,9 +16,32 @@ endif()
 # Expand relative path. This is important if the provided path contains a tilde (~)
 get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
-file(DOWNLOAD
-     https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-     ${CPM_DOWNLOAD_LOCATION} EXPECTED_HASH SHA256=${CPM_HASH_SUM}
-)
+if(EXISTS "${CPM_DOWNLOAD_LOCATION}")
+  include("${CPM_DOWNLOAD_LOCATION}")
+  return()
+endif()
+
+file(
+  DOWNLOAD
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
+  ${CPM_DOWNLOAD_LOCATION}
+  EXPECTED_HASH SHA256=${CPM_HASH_SUM}
+  STATUS CPM_DOWNLOAD_STATUS
+  LOG CPM_DOWNLOAD_LOG)
+
+list(GET CPM_DOWNLOAD_STATUS 0 CPM_DOWNLOAD_RESULT)
+if(NOT CPM_DOWNLOAD_RESULT EQUAL 0)
+  if(NOT BUILD_TESTING AND NOT WATPOCKET_BUILD_CLI)
+    message(WARNING "CPM.cmake download failed; using offline no-op fallback for wheel builds")
+
+    function(cpmaddpackage)
+      message(FATAL_ERROR "CPMAddPackage is unavailable in the offline fallback")
+    endfunction()
+
+    return()
+  endif()
+
+  message(FATAL_ERROR "Failed to download CPM.cmake: ${CPM_DOWNLOAD_LOG}")
+endif()
 
 include(${CPM_DOWNLOAD_LOCATION})
